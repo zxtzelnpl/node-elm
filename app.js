9bf9f02e7bb0ef4029a9854ed3d5bc397d1a1295
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-06-09 14:10:06 +0800

    1

--- app.js
+++ app.js
@@ -14,30 +14,30 @@ import Statistic from './middlewares/statistic'
 const app = express();
 
 app.all('*', (req, res, next) => {
-	res.header("Access-Control-Allow-Origin", req.headers.origin);
-	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
-	res.header("X-Powered-By", '3.2.1')
-	if (req.method == 'OPTIONS') {
-	  	res.send(200);
-	} else {
-	    next();
-	}
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+  res.header("X-Powered-By", '3.2.1')
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 app.use(Statistic.apiRecord)
 const MongoStore = connectMongo(session);
 app.use(cookieParser());
 app.use(session({
-	  	name: config.session.name,
-		secret: config.session.secret,
-		resave: true,
-		saveUninitialized: false,
-		cookie: config.session.cookie,
-		store: new MongoStore({
-	  	url: config.url
-	})
+  name: config.session.name,
+  secret: config.session.secret,
+  resave: true,
+  saveUninitialized: false,
+  cookie: config.session.cookie,
+  store: new MongoStore({
+    url: config.url
+  })
 }))
 
 // app.use(expressWinston.logger({
@@ -70,7 +70,7 @@ router(app);
 app.use(history());
 app.use(express.static('./public'));
 app.use((err, req, res, next) => {
-	res.status(404).send('未找到当前路由');
+  res.status(404).send('未找到当前路由');
 });
 
 
